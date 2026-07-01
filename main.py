3549d4d9de08d05ee372e5727c347ddf37cf6222
Ysabelle Ablir <ysabelleablir@gmail.com>
2026-07-01 17:36:10 +0000

    added webhook

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
+
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return render_template('landing.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
